75361b6f4278111d7f8a6cf889dd689dd186014a
Mariusz Najwer <najwer23@gmail.com>
2019-06-24 20:30:52 +0200

    menu and header, version 1.0

---
+++ public/JS/menu/menu.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    const toggler = document.querySelector('.menu__toggler');
+    const menu = document.querySelector('.menu');
+
+    /*
+     * Toggles on and off the 'active' class on the menu
+     * and the toggler button.
+     */
+
+    if (toggler) {
+        toggler.addEventListener('click', () => {
+            toggler.classList.toggle('active');
+            menu.classList.toggle('active');
+        })
+    }
+
+    console.log("df");
+}
+
